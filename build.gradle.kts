449619983f0ad86f3d47917257a65594ec730cd2
Sravan Kumar Katta <sravan-kumar.katta@obi-extern.com>
2026-05-20 16:24:19 +0200

    fixing versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
-	kotlin("jvm") version "2.2.21"
-	kotlin("plugin.spring") version "2.2.21"
-	id("org.springframework.boot") version "4.0.6"
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+	id("org.springframework.boot") version "3.5.12"
+	id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "kotlin"
